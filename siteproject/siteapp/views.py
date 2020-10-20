b59f003592ec654dd0ed5604728843522ecdc480
Aditya Bose <13054902+adbose@users.noreply.github.com>
2020-10-20 20:59:10 +0530

    Update site templates

--- siteproject/siteapp/views.py
+++ siteproject/siteapp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 # Create your views here.
+# Home view
+class HomePageView(TemplateView):
+    def get(self, request, **kwargs):
+        return render(request, 'index.html', context=None)
+
+
+# About view
+class AboutPageView(TemplateView):
+    template_name = 'about.html'  # this is another way of using TemplateView
